5e6dce6d786d8958ad742e3f76f7ca360adbc2ce
Ignacio Gallardo <GallardojsDeveloper@gmail.com>
2024-02-06 20:34:24 -0300

    add socket io

--- socket.js
+++ socket.js
@@ -1,16 +1,17 @@
-const { Socket } = require('engine.io');
-
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-
 io.on('connection', (socket) => {
+    console.log('Nueva conexión');
 
+    socket.on('saltar', () =>{
+        socket.broadcast.emit('jugador-saltando');
+    });
 });
 
 var port = 2525;
 
-http.listen(port, function(){
-    console.log('Se esta escuchando localhost:'+port);
+http.listen(port, function() {
+    console.log('listening in http://localhost:' + port);
 });
